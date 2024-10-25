f13a9e5fd32bdc1e5ce547c8d85b877557581384
isurojit <iamsurojit@hotmail.com>
2024-10-25 22:39:51 +0530

    Portfolio about corrected

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,15 +1,23 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
+import hero from "./hero.jpg";
+import meta from "./meta.png";
+import shopify from "./shopify.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+import logo from "./logo.svg";
+import godara from "./godara.png";
+import partypropz from "./partypropz.png";
+import tsolution from "./tsolution.png";
+import freelance from "./freelance.png";
 
 export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    logo
-}
+  hero,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  logo,
+  godara,
+  partypropz,
+  tsolution,
+  freelance,
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 import { logo } from "../assets/images";
 
 const Navbar = () => {
+  // Path to your CV PDF file
+  const cvUrl = "/src/assets/CV_SUROJIT_MANNA.pdf";
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = cvUrl;
+    link.download = "SUROJIT_MANNA_CV.pdf"; // The name it will save as
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <header className="header">
       <NavLink
@@ -27,6 +39,15 @@ const Navbar = () => {
           }
         >
           Projects
+        </NavLink>
+        <NavLink
+          to="#"
+          onClick={handleDownload}
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          Download CV
         </NavLink>
       </nav>
     </header>
